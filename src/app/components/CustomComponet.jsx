26ad9636589b33c6bdab7da33c7f46f67ae9cf14
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-20 21:15:58 +0500

    custom

---
+++ src/app/components/CustomComponet.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomComponet = () => {
+    return (
+        <div>CustomComponet</div>
+    )
+}
+
+export default CustomComponet
--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,24 @@
-import ContactUs from "./components/contactus/ContactUs";
-import Layout from "./components/layout/Layout";
-import Hero from "./components/hero/Hero";
-import Work from "./components/work/Work";
-import Service from "./components/service/Service";
-import Team from "./components/team/Team";
+// import ContactUs from "./components/contactus/ContactUs";
+// import Layout from "./components/layout/Layout";
+// import Hero from "./components/hero/Hero";
+// import Work from "./components/work/Work";
+// import Service from "./components/service/Service";
+
+import CustomComponet from "./components/CustomComponet";
+
+// import Team from "./components/team/Team";
 export default function Home() {
   return (
     <>
+      <CustomComponet />
 
-      <Layout>
+      {/* <Layout>
         <Hero />
         <Work />
         <Service />
         <Team />
         <ContactUs />
-      </Layout>
+      </Layout> */}
     </>
   );
 }
