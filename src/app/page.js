833d67046c7876c1e15f49de8aca927130a097c9
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-20 12:05:25 +0500

    comp

--- src/app/page.js
+++ src/app/page.js
@@ -270,6 +270,19 @@ export default function Home() {
         </div>
       </div>
       {/* team  section end*/}
+      {/* contact us start */}
+      <div className="container mx-auto">
+        <div>
+
+          <h2 className="text-center">contact us</h2>
+          <div className="d-flex justify-content-center align-items-center flex-column text-capitalize ">
+
+            <p><b>email : </b>sharjeelalibari@gmail.com</p>
+            <p><b>phone : </b>+9234567890</p>
+          </div>
+        </div>
+      </div>
+      {/* contact us end */}
       {/* footer  start*/}
       <div className="bg-danger">
         <div className="container mx-auto">
