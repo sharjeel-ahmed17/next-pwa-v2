83081cf2e8b32f656f5de24ce83b29e6d0a45c9e
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-20 21:40:06 +0500

    add

--- src/app/components/CustomComponet.jsx
+++ src/app/components/CustomComponet.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import Image from "next/image"
+
+import image1 from "/public/image/1.png"
+import image2 from "/public/image/2.png"
+import image3 from "/public/image/3.png"
+
+
+export const customDataList = [
+    {
+        imgUrl: image1
+    },
+    {
+        imgUrl: image2
+    },
+    {
+        imgUrl: image3
+    },
+
+]
+
 
 const CustomComponet = () => {
     return (
-        <div>CustomComponet</div>
+        <div>
+
+
+            {
+                customDataList.map((item, index) => {
+                    const { imgUrl } = item;
+                    return <div key={index}>
+                        <Image
+                            src={imgUrl}
+                            width={200}
+                            height={200}
+                            alt={imgUrl}
+                        />
+                    </div>
+
+                })
+            }
+
+        </div>
     )
 }
 
---
+++ src/app/components/data.jsx
@@ -0,0 +1,15 @@
+
+
+
+// export const customDataList = [
+//     {
+//         imgUrl: "/public/image/1.png"
+//     },
+//     {
+//         imgUrl: "/public/image/4.jpeg"
+//     },
+//     {
+//         imgUrl: "/public/image/1.png"
+//     },
+
+// ]
