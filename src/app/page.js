dc719f355177847769d4494ebbf1849732c9ca40
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-21 14:57:47 +0500

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import ContactUs from "./components/contactus/ContactUs";
-import Layout from "./components/layout/Layout";
-import Hero from "./components/hero/Hero";
-import Work from "./components/work/Work";
-import Service from "./components/service/Service";
-import Team from "./components/team/Team";
+// import ContactUs from "./components/contactus/ContactUs";
+// import Layout from "./components/layout/Layout";
+// import Hero from "./components/hero/Hero";
+// import Work from "./components/work/Work";
+// import Service from "./components/service/Service";
+// import Team from "./components/team/Team";
 
 
 
@@ -14,14 +14,14 @@ export default function Home() {
       {/* updation is here */}
       {/* update */}
       {/* update the current page */}
-
-      <Layout>
+      <p>hello world how are you</p>
+      {/* <Layout>
         <Hero />
         <Work />
         <Service />
         <Team />
         <ContactUs />
-      </Layout>
+      </Layout> */}
     </>
   );
 }
