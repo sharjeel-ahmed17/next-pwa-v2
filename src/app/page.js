da6873fd274cd73cd80d6519f8927f5ce42001df
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-21 15:25:16 +0500

    done

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,48 @@
 // import Service from "./components/service/Service";
 // import Team from "./components/team/Team";
 
+import Image from "next/image";
 
+import WORK_IMG_1 from "/public/image/1.png"
+import WORK_IMG_2 from "/public/image/2.png"
+import WORK_IMG_3 from "/public/image/3.png"
+import WORK_IMG_4 from "/public/image/4.jpeg"
+const workData = [
+  {
+    imageUrl: WORK_IMG_1,
+    title: 'Graphic Design',
+    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+    href: "https://chatgpt.com/",
+    link: "Chatgpt",
+    alt: "this is my image "
 
+  },
+  {
+    imageUrl: WORK_IMG_2,
+    title: 'Seo',
+    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+    href: "https://chatgpt.com/",
+    link: "gemini",
+    alt: "this is my image "
+  },
+  {
+    imageUrl: WORK_IMG_3,
+    title: 'Web Development',
+    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+    href: "https://chatgpt.com/",
+    link: "leonardo",
+    alt: "this is my image "
+  },
+  {
+    imageUrl: WORK_IMG_4,
+    title: 'Wordpress',
+    description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+    href: "https://chatgpt.com/",
+    link: "bing ai",
+    alt: "this is my image "
+  },
+
+];
 
 export default function Home() {
   return (
@@ -41,7 +81,7 @@ export default function Home() {
             <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="#">
                   Home
@@ -52,60 +92,67 @@ export default function Home() {
                   Link
                 </a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
+
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Link
                 </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
               </li>
               <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">
-                  Disabled
+                <a className="nav-link" href="#">
+                  Link
                 </a>
               </li>
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+
           </div>
         </div>
       </nav>
 
       {/* navbar end */}
-      {/* update */}
+
+      {/* work start */}
+      <div className="container mx-auto my-5">
+        <div className="row">
+
+
+          {
+            workData.map((item, index) => {
+              const { imageUrl, title, link, href, description, alt } = item;
+              return (
+                <div className="col-md-6 p-2" key={index}>
+                  <Image
+                    src={imageUrl}
+                    alt={alt}
+                    style={{
+                      width: "100%",
+                      height: "400px"
+                    }}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+      {/* work end */}
+      {/* contact us start*/}
+      <div className="container mx-auto">
+        <h3 className="text-center"><b>email : </b> sharjeelalibari3@gmail.com</h3>
+        <h3 className="text-center"><b>phone :</b>03426307</h3>
+
+      </div>
+      {/* contact us end */}
+      {/* footer start */}
+      <div className="bg-primary">
+        <div className="container">
+          <footer className="text-center py-4 text-white text-uppercase ">
+            all right reversed by sharjeel ahmed
+          </footer>
+        </div>
+
+      </div>
+      {/* footer end */}
     </>
   );
 }
