d4ce4ab0f43335de3f21e6e673f5f7cd17e845b2
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-05-21 14:51:27 +0500

    some

--- src/app/page.js
+++ src/app/page.js
@@ -1,41 +1,25 @@
-// import ContactUs from "./components/contactus/ContactUs";
-// import Layout from "./components/layout/Layout";
-// import Hero from "./components/hero/Hero";
-// import Work from "./components/work/Work";
-// import Service from "./components/service/Service";
+import ContactUs from "./components/contactus/ContactUs";
+import Layout from "./components/layout/Layout";
+import Hero from "./components/hero/Hero";
+import Work from "./components/work/Work";
+import Service from "./components/service/Service";
+import Team from "./components/team/Team";
 
-import Image from "next/image";
 
-// import CustomComponet from "./components/CustomComponet";
 
-// import Team from "./components/team/Team";
+
 export default function Home() {
   return (
     <>
-      {/* <CustomComponet /> */}
-      <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <Image
-              src={'/public/image/1.png'}
-              width={200}
-              height={200}
-              alt="some image"
 
-            />
-            {/* here is the updated code of next js */}
-          </div>
-        </div>
-      </div>
-      {/* jab tak page componet update nahi hoga */}
 
-      {/* <Layout>
+      <Layout>
         <Hero />
         <Work />
         <Service />
         <Team />
         <ContactUs />
-      </Layout> */}
+      </Layout>
     </>
   );
 }
